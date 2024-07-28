49330dc54a538c1eaa4b6f9716ce68939df1772a
POORNIMA <kumawatpoornima7@gmailcom>
2024-07-28 12:36:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const val = require("./conn");
+const mongoose = require("mongoose");
+const User = require("./schema");
+const cors = require("cors");
+
+const app = express();
+
+// Connect to the database
+val();
+app.use(
+  cors({
+    origin: "localhost:3000",
+  })
+);
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/submit", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.validate();
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    const errorMessages = {};
+    if (error.errors) {
+      for (const key in error.errors) {
+        errorMessages[key] = error.errors[key].message;
+      }
+    } else {
+      errorMessages.general = error.message;
+    }
+    res
+      .status(400)
+      .send({ message: "Error saving user", errors: errorMessages });
+  }
+});
+
+// app.get("/submit", async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     res.status(200).send(users);
+//   } catch (error) {
+//     res
+//       .status(400)
+//       .send({ message: "Error fetching users", error: error.message });
+//   }
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
